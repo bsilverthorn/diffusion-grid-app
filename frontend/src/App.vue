1fd0e242a9913367d6e022e80dd7dc5651d86db4
Bryan Silverthorn <bsilverthorn@gmail.com>
2022-12-03 20:09:04 +0000

    frontend: extract Controls and Trajectory.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
 <script>
 import { useGridStore } from './stores/grid';
 import GridImage from './GridImage.vue';
+import Controls from './Controls.vue';
+import Trajectory from './Trajectory.vue';
 
 export default {
     name: 'App',
     components: {
         GridImage,
+        Controls,
+        Trajectory,
     },
     setup: () => ({
         gridStore: useGridStore(),
@@ -38,49 +42,8 @@ export default {
 
 <!-- latents sidebar -->
 <div class="row-start-1 row-span-3 col-start-1">
-    <div class="grid grid-rows-15 grid-cols-5 h-full">
-        <template v-for="(latentsInfo, i) in gridStore.trunk.trajectory?.values()" :key="i">
-            <img
-                :src="`data:image/jpeg;base64,${hoveredBranch?.trajectory.get(latentsInfo.timestep)?.image ?? latentsInfo.image}`"
-                :title="`t = ${latentsInfo.timestep}`"
-                :class="`row-start-${2 + i * 5}`"
-                class="row-span-3 col-start-2 col-span-3">
-            <div v-if="i == 0" class="row-start-1 col-start-3 grid place-items-center">
-                <!-- tall vertical ellipsis -->
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 42" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[4vmin] relative top-[-2vmin]">
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5z
-                           M12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z
-                           M12 30.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-                </svg>
-             </div>
-            <div v-if="i > 0" :class="`row-start-${i * 5}`" class="row-span-2 col-start-3 grid place-items-center">
-                <!-- tall vertical ellipsis -->
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 42" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[4vmin]">
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5z
-                           M12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z
-                           M12 30.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-                </svg>
-            </div>
-            <div :class="`row-start-${3 + i * 5}`" class="col-start-5 text-2xl grid place-items-center">
-                <!-- right arrow -->
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[2vmin]">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                </svg>
-            </div>
-            <div v-if="i == gridStore.trunk.trajectory.size - 1" :class="`row-start-${5 + i * 5}`" class="row-span-2 col-start-3 text-2xl grid place-items-center">
-                <!-- down arrow -->
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[2vmin]">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
-                </svg>
-            </div>
-        </template>
-    </div>
+    <trajectory :hovered="hoveredBranch">
+    </trajectory>
 </div>
 
 <!-- grid images -->
@@ -111,32 +74,8 @@ export default {
     <div class="grid place-items-center h-full text-slate-700 leading-relaxed text-center text-[3vmin] p-[2vmin] font-serif">
         “{{ gridStore.prompt.text }}”
     </div>
-    <div class="absolute bottom-0 right-0">
-        <button type="button" @click="gridStore.changePrompt(-1)">
-            <!-- left chevron -->
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[2vmin] hover:stroke-purple-600">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-            </svg>
-        </button>
-        <button type="button" class="ml-[0.20vmin]" @click="gridStore.reseedAll()">
-            <!-- refresh symbol -->
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[2vmin] hover:stroke-purple-600">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-            </svg>
-        </button>
-        <a href="https://github.com/bsilverthorn/diffusion-grid-app#diffusion-grid" target="_blank" class="ml-[0.25vmin] inline-block">
-            <!-- info symbol -->
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[2vmin] hover:stroke-purple-600">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-            </svg>
-        </a>
-        <button type="button" @click="gridStore.changePrompt(1)">
-            <!-- right chevron -->
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[2vmin] hover:stroke-purple-600">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg>
-        </button>
-    </div>
+    <controls class="absolute bottom-0 right-0">
+    </controls>
 </div>
 
 </div>
---
+++ frontend/src/Trajectory.vue
@@ -0,0 +1,60 @@
+<script>
+import { useGridStore } from './stores/grid';
+
+export default {
+    setup: () => ({
+        gridStore: useGridStore(),
+    }),
+    props: {
+        hovered: Object,
+    },
+};
+</script>
+
+<template>
+<div class="grid grid-rows-15 grid-cols-5 h-full">
+
+<template v-for="(latentsInfo, i) in gridStore.trunk.trajectory?.values()" :key="i">
+    <img
+        :src="`data:image/jpeg;base64,${hovered?.trajectory.get(latentsInfo.timestep)?.image ?? latentsInfo.image}`"
+        :title="`t = ${latentsInfo.timestep}`"
+        :class="`row-start-${2 + i * 5}`"
+        class="row-span-3 col-start-2 col-span-3">
+    <div v-if="i == 0" class="row-start-1 col-start-3 grid place-items-center">
+        <!-- tall vertical ellipsis -->
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 42" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[4vmin] relative top-[-2vmin]">
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5z
+                    M12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z
+                    M12 30.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+        </svg>
+        </div>
+    <div v-if="i > 0" :class="`row-start-${i * 5}`" class="row-span-2 col-start-3 grid place-items-center">
+        <!-- tall vertical ellipsis -->
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 42" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[4vmin]">
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5z
+                    M12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z
+                    M12 30.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+        </svg>
+    </div>
+    <div :class="`row-start-${3 + i * 5}`" class="col-start-5 text-2xl grid place-items-center">
+        <!-- right arrow -->
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[2vmin]">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+        </svg>
+    </div>
+    <div v-if="i == gridStore.trunk.trajectory.size - 1" :class="`row-start-${5 + i * 5}`" class="row-span-2 col-start-3 text-2xl grid place-items-center">
+        <!-- down arrow -->
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[2vmin] h-[2vmin]">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+        </svg>
+    </div>
+</template>
+
+</div>
+</template>
