e9a3bd606627810b0978ebafcb63c2cc635bbd99
Bryan Silverthorn <bsilverthorn@gmail.com>
2022-12-03 19:35:21 +0000

    frontend/api: provide higher-level fetch calls.
    API interface can encapsulate more call details.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,8 +5,10 @@ export class DGAPI {
     }
 
     call(method, path, data, options = {}) {
+        const args = new URLSearchParams(options.args).toString();
+
         return fetch(
-            this.apiRoot + path,
+            this.apiRoot + path + (args !== '' ? '?' + args : ''),
             {
                 method: method,
                 headers: {
@@ -14,7 +16,7 @@ export class DGAPI {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(data),
-                ...options,
+                signal: options.signal,
             },
         )
         .then(response => {
@@ -30,10 +32,7 @@ export class DGAPI {
         })
         .then(response => response.json())
         .catch(error => {
-            if(error.name === 'AbortError') {
-                console.log("aborted request to", path);
-            }
-            else {
+            if(error.name !== 'AbortError') {
                 this.errorListener(error);
             }
 
@@ -47,5 +46,77 @@ export class DGAPI {
 
     post(path, data, options) {
         return this.call('POST', path, data, options);
+    }
+
+    fetchPrompts() {
+        return this.get('/prompts');
+    }
+
+    fetchBranch(prompt, latents, timestep, seed, trajectoryAt) {
+        const abort = new AbortController();
+
+        function check(callId) {
+            return this.get(
+                `/diffusions/${callId}`,
+                {
+                    signal: abort.signal,
+                },
+            )
+            .then((data) => {
+                if(data['call_id'] !== undefined) {
+                    const timeout = new Promise(resolve => setTimeout(resolve, 1600));
+
+                    return timeout.then(() => check(callId));
+                }
+                else {
+                    return data;
+                }
+            });
+        };
+
+        const requestBody = {
+            'prompt': prompt.text,
+            'seed': seed,
+            'latents': latents?.tensor,
+            'timestep': timestep,
+            'trajectory_at': trajectoryAt,
+        };
+        const requestArgs = {
+            'signature': latents?.signature ?? prompt.signature,
+        };
+        const request = this.post(
+            '/diffusions',
+            requestBody,
+            {
+                args: requestArgs,
+                signal: abort.signal,
+            },
+        )
+        .then((data) => {
+            if(data['call_id'] !== undefined) {
+                return check.bind(this)(data['call_id'], abort.signal);
+            }
+            else {
+                return data;
+            }
+        })
+        .then((data) => {
+            return {
+                image: data['diffusion']['image'],
+                trajectory: new Map(
+                    data['diffusion']['trajectory'].map(
+                        info => [
+                            info['timestep'],
+                            {
+                                ...info,
+                                signature: data['signatures'][info['timestep']],
+                            },
+                        ],
+                    ),
+                ),
+            }
+        });
+
+        return { abort, request };
     }
 }
